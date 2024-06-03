6dac8d77bd0defe6004033b328764d1b127ba002
MonicaSherley <monicasherley789@gmail.com>
2024-06-03 19:03:48 +0530

    New Folder-1

---
+++ XYZ.py
@@ -0,0 +1,11 @@
+def emoji_converter(message):
+    words = message.split(" ")
+    emojis = {
+    ":)" : "😁",
+    ":(" : "😖",
+    ":}" : "😭"  
+    }
+    output = ""
+    for word in words:
+        output += emojis.get(word, word)+ " "
+    return output
